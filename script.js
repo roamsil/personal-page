24121ce1dc27b95b391db19070e434b6098158b2
roamsil <romarioamparo@gmail.com>
2024-09-11 09:55:05 -0300

    change background on media queries

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 function toggleMode() {
   const html = document.documentElement
-  html.classList.toggle('light')
+  html.classList.toggle("light")
 
   // pegar a tag img
-  const img = document.querySelector('#profile img')
+  const img = document.querySelector("#profile img")
 
   // substituir a imagem
-  if(html.classList.contains('light')) {
+  if (html.classList.contains("light")) {
     // se tiver light mode, adicionar imagem light
-    img.setAttribute('src', './assets/avatar-light.png')
-    img.setAttribute('alt', 'Romario A.Silva olhar fixo e netrante com luvas de luta suado em tatame de camisa preta.')
- }
- else {
-   // se tiver sem light mode, manter a imagem normal
-     img.setAttribute('src', './assets/avatar.png')
-     img.setAttribute(
-       "alt", "Foto de Romario A.Silva de perfil com fundo cinza, serio e camisa cinza."
-     )
-    }
-
-
-} 
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute(
+      "alt",
+      "Romario A.Silva olhar fixo e netrante com luvas de luta suado em tatame de camisa preta."
+    )
+  } else {
+    // se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Romario A.Silva de perfil com fundo cinza, serio e camisa cinza."
+    )
+  }
+}
